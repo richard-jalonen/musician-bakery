ad06b3055883c1320aaed32b213aa3ff9b69e402
Richard Jalonen <jalonen.richard@gmail.com>
2020-09-18 21:59:27 +0300

    backend initial commit

---
+++ backend/src/main/java/ee/richja/musicianbakery/MusicianBakeryApplication.java
@@ -0,0 +1,13 @@
+package ee.richja.musicianbakery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicianBakeryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicianBakeryApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/ee/richja/musicianbakery/MusicianBakeryApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.richja.musicianbakery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicianBakeryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
