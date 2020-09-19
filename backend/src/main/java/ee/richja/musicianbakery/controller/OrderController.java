688b9afce51abbe0469858df9e937e2cde88c221
Richard Jalonen <jalonen.richard@gmail.com>
2020-09-19 17:02:56 +0300

    changed endpoint address mappings and updated the readme

--- backend/src/main/java/ee/richja/musicianbakery/controller/OrderController.java
+++ backend/src/main/java/ee/richja/musicianbakery/controller/OrderController.java
@@ -7,14 +7,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/order")
 public class OrderController {
     @Autowired
     OrderService orderService;
 
-    @GetMapping("/order/{orderRequest}")
+    @GetMapping("/{orderRequest}")
     public ResponseEntity getOrder(@PathVariable String orderRequest) {
         if (!orderService.isValidInput(orderRequest))
             return new ResponseEntity<>("Invalid order input", HttpStatus.BAD_REQUEST);
--- backend/src/main/java/ee/richja/musicianbakery/controller/PaymentController.java
+++ backend/src/main/java/ee/richja/musicianbakery/controller/PaymentController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.math.BigDecimal;
 
 @RestController
+@RequestMapping("/payment")
 public class PaymentController {
     @Autowired
     PaymentService paymentService;
